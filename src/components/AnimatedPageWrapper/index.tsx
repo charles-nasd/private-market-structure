e7a03eeffad458f1bfd0a5e12b260b6c3d3a927f
charles-nasd <cejiegbu@nasdng.com>
2023-03-15 14:16:05 +0100

    feat: added animated page component

---
+++ src/components/AnimatedPageWrapper/index.tsx
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+
+const animations = {
+  initial: { opacity: 0, x: 100 },
+  animate: { opacity: 1, x: 0 },
+  exit: { opacity: 0, x: -100 },
+};
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const AnimatedPageWrapper: React.FC<Props> = ({ children }) => {
+  return (
+    <motion.div
+      variants={animations}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{ duration: 0.4, ease: 'easeInOut' }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default AnimatedPageWrapper;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Layout } from './Layout';
 export { default as Preloader } from './Preloader';
 export { default as RequireAuth } from './RequireAuth';
+export { default as AnimatedPageWrapper } from './AnimatedPageWrapper';
