ce8eee187020a03e9ed08908994950bc7361d87f
charles-nasd <cejiegbu@nasdng.com>
2023-03-15 11:45:15 +0100

    feat: added pages

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as LandingPage } from './LandingPage';
+export { default as Login } from './Login';
+export { default as Portal } from './Portal';
+export { default as RouteNotFound } from './RouteNotFound';
